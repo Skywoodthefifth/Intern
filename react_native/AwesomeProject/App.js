a8f3b626afd0671f735230ac2e5f670f6981cb3d
Vo Huu Nam Truong <namtruong0900@gmail.com>
2024-01-24 10:05:52 +0700

    Update App.js

--- react_native/AwesomeProject/App.js
+++ react_native/AwesomeProject/App.js
@@ -1,32 +1,62 @@
 import React, { useState } from "react";
-import { Button, Text, View } from "react-native";
+import { StyleSheet, Text, FlatList, View, Pressable } from "react-native";
 
-const Cat = (props) => {
-  const [isHungry, setIsHungry] = useState(true);
+export default function App() {
+  const [people, setPeople] = useState([
+    { name: "shaun", id: "1" },
+    { name: "yoshi", id: "2" },
+    { name: "mario", id: "3" },
+    { name: "luigi", id: "4" },
+    { name: "peach", id: "5" },
+    { name: "toad", id: "6" },
+    { name: "bowser", id: "7" },
+  ]);
+
+  const pressHandler = (id) => {
+    console.log(id);
+    setPeople((prevPeople) => {
+      return prevPeople.filter((person) => person.id != id);
+    });
+  };
 
   return (
-    <View>
-      <Text>
-        I am {props.name}, and I am {isHungry ? "hungry" : "full"}!
-      </Text>
-      <Button
-        onPress={() => {
-          setIsHungry(false);
-        }}
-        disabled={!isHungry}
-        title={isHungry ? "pour me some milk pls" : "Thank you!"}
+    <View style={styles.container}>
+      <FlatList
+        numColumns={2}
+        keyExtractor={(item) => item.id}
+        data={people}
+        renderItem={({ item }) => (
+          <Pressable onPress={() => pressHandler(item.id)}>
+            <Text style={styles.item}>{item.name}</Text>
+          </Pressable>
+        )}
       />
+
+      {/* <ScrollView>
+        {people.map((item) => (
+          <View key={item.key}>
+            <Text style={styles.item}>{item.name}</Text>
+          </View>
+        ))}
+      </ScrollView> */}
     </View>
   );
-};
+}
 
-const Cafe = () => {
-  return (
-    <>
-      <Cat name="Munkustrap" />
-      <Cat name="Spot" />
-    </>
-  );
-};
-
-export default Cafe;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingTop: 40,
+    paddingBottom: 20,
+    //alignItems: "center",
+    //justifyContent: "center",
+  },
+  item: {
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: "pink",
+    fontSize: 24,
+    marginHorizontal: 10,
+  },
+});
